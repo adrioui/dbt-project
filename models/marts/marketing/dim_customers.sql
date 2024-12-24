0588adbc6d59a262bfef38f9b93c5acd3cc29525
Adri Fadilah <adrifdlh@gmail.com>
2024-12-24 05:40:49 +0000

    refactored dim_customers to add two staging models. set project configuration to materialize all marts as table and all staging as view

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,7 +1,7 @@
 with
-    customers as (select * from {{ ref("stg_customers") }}),
+    customers as (select * from {{ ref("stg_jaffle_shop__customers") }}),
 
-    orders as (select * from {{ ref("stg_orders") }}),
+    orders as (select * from {{ ref("stg_jaffle_shop__orders") }}),
 
     customer_orders as (
 
