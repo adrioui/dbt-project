f1ed03a6daf02c3541fc7e0c966f0efb4eddbf53
Adri Fadilah <adrifdlh@gmail.com>
2024-12-26 04:04:51 +0000

    add documentation to jaffle shop and stripe

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,10 +1,8 @@
 select
-    id,
+    id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
+    amount,
     created as created_at
-
-from raw.stripe.payment
+from {{ source("stripe", "payment") }}
